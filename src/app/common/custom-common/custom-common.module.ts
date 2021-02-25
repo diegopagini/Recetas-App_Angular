b87eda499305bcec655f195b53650488368ad0b0
Diego Pagini <dq@qbkconsulting.com>
2021-02-25 08:15:34 -0300

    navbar

--- src/app/common/custom-common/custom-common.module.ts
+++ src/app/common/custom-common/custom-common.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/app/material/material/material.module';
+import { NavbarModule } from '../navbar/navbar.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
-  exports: [],
+  imports: [CommonModule, MaterialModule, NavbarModule],
+  exports: [NavbarModule],
 })
 export class CustomCommonModule {}
---
+++ src/app/common/navbar/navbar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar.component';
+import { MaterialModule } from 'src/app/material/material/material.module';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [NavbarComponent],
+})
+export class NavbarModule {}
