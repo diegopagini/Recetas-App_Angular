604415d59c847a51050aaa06e9074969f6a60825
Diego Pagini <dq@qbkconsulting.com>
2021-04-03 08:05:32 -0300

    cambio de icono y titulo

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  scrollToBTN() {
+    const btn = document.getElementById('modal-trigger');
+    const header = document.getElementById('option-container');
+    const headerOffset = header.offsetHeight;
+    const btnOffset = btn.offsetTop;
 
-  constructor() { }
+    const offsetPosition = btnOffset - headerOffset;
 
-  ngOnInit(): void {
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
   }
-
 }
